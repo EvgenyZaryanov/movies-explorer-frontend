67994f5ebc2e2d60f84a6f2bbb62f5a73d080c52
Evgeny Zaryanov <eugenezaryanov@yandex.ru>
2023-12-27 18:56:14 +0300

    new updates

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,26 +8,32 @@ function Footer() {
     !hiddenRoutes.includes(location.pathname) &&
     location.pathname !== '/profile' && (
       <footer className="footer">
-        <p className="footer__info">Учебный проект Яндекс.Практикум х BeatFilm.</p>
-        <div className="footer__more-info">
-          <p className="footer__copyright">&copy; 2023</p>
-          <div className="footer__links">
-            <a
-              className="footer__link"
-              href="https://practicum.yandex.ru/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Яндекс.Практикум
-            </a>
-            <a
-              className="footer__link"
-              href="https://github.com/Yandex-Practicum"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
+        <div className="footer__container">
+          <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+          <div className="footer__info">
+            <p className="footer__year">© 2023</p>
+            <ul className="footer__links">
+              <li className="footer__link">
+                <a
+                  className="footer__link-config"
+                  href="https://practicum.yandex.ru/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Яндекс.Практикум
+                </a>
+              </li>
+              <li className="footer__link">
+                <a
+                  className="footer__link-config"
+                  href="https://github.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </footer>
